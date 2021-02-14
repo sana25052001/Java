3605d2379e8c9431d604242f6aec638bc5ae980f
sana25052001 <75016219+sana25052001@users.noreply.github.com>
2021-02-14 16:44:36 +0530

    Updated conversion.java
    In above code it will ask the user for the number of minutes and would give the output as the number of years and days respectively.

--- conversion.java
+++ conversion.java
@@ -1,18 +1,23 @@
-import java.util.*;
-public class pub {
+package code1;
 
-	public static void main(String[] args) 
-	{
-		Scanner s=new Scanner(System.in);
-		int a;
-		a=s.nextInt();
-		int year,days;
-		int cn=60*24*365;
-		year=a/cn;
-		System.out.print(year+" years ");
-		a=a-(year*cn);
-		int cm=60*24;
-		days=a/cm;
-		System.out.print("and "+days+" days.");
+import java.util.Scanner;
+
+public class Code2 {
+
+		public static void main(String[] args) 
+		{
+			Scanner s=new Scanner(System.in);
+			int a;
+			System.out.println("enter numbers.");
+			a=s.nextInt();
+			int year,days;
+			int cn=60*24*365;
+			year=a/cn;
+			System.out.print(year+" years ");
+			a=a-(year*cn);
+			int cm=60*24;
+			days=a/cm;
+			System.out.print("and "+days+" days.");
+		}
 	}
-}
+	
